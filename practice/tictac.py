f094b4d736765a396a7b16dc9be1ce82b5648567
Kishan Wali <118239409+Nahsik16@users.noreply.github.com>
2024-10-19 00:26:33 +0530

    Update tictac.py

--- practice/tictac.py
+++ practice/tictac.py
@@ -1,130 +1,66 @@
 import math
 
-# Function to display the board
-def display_board(board):
-    print("Current board state:")
-    for row in board:
-        print("|".join(row))
-    print("\n")
-
-# Function to check if the game has ended in a win
+# Check if the game has ended in a win
 def check_winner(board):
     win_conditions = [
-        [board[0][0], board[0][1], board[0][2]],
+        [board[0][0], board[0][1], board[0][2]],  # Rows
         [board[1][0], board[1][1], board[1][2]],
         [board[2][0], board[2][1], board[2][2]],
-        [board[0][0], board[1][0], board[2][0]],
+        [board[0][0], board[1][0], board[2][0]],  # Columns
         [board[0][1], board[1][1], board[2][1]],
         [board[0][2], board[1][2], board[2][2]],
-        [board[0][0], board[1][1], board[2][2]],
+        [board[0][0], board[1][1], board[2][2]],  # Diagonals
         [board[0][2], board[1][1], board[2][0]]
     ]
     if ['X', 'X', 'X'] in win_conditions:
         return 'X'
     elif ['O', 'O', 'O'] in win_conditions:
         return 'O'
-    else:
-        return None
+    return None
 
-# Function to check if the game is over (either win or draw)
+# Check if the game is over (either win or draw)
 def game_over(board):
     return check_winner(board) or all(cell != ' ' for row in board for cell in row)
 
-# Evaluation function: +1 for 'X' win, -1 for 'O' win, 0 for draw
+# Evaluate position: +1 for 'X' win, -1 for 'O' win, 0 for draw
 def evaluate_position(board):
     winner = check_winner(board)
-    if winner == 'X':
-        return 1
-    elif winner == 'O':
-        return -1
-    else:
-        return 0
+    return 1 if winner == 'X' else -1 if winner == 'O' else 0
 
 # Get available moves (empty spots on the board)
-def get_children(board):
-    children = []
-    for i in range(3):
-        for j in range(3):
-            if board[i][j] == ' ':
-                new_board = [row[:] for row in board]  # Deep copy the board
-                children.append((new_board, i, j))
-    return children
+def get_children(board, player):
+    return [([row[:] for row in board], i, j) for i in range(3) for j in range(3) if board[i][j] == ' ']
 
 # Minimax function
-def minimax(board, depth, maximizingPlayer):
+def minimax(board, depth, maximizing_player):
     if depth == 0 or game_over(board):
         return evaluate_position(board)
-    if maximizingPlayer:
-        maxEval = -math.inf
-        for child, i, j in get_children(board):
-            child[i][j] = 'X'
-            eval = minimax(child, depth - 1, False)
-            maxEval = max(maxEval, eval)
-        return maxEval
-    else:
-        minEval = math.inf
-        for child, i, j in get_children(board):
-            child[i][j] = 'O'
-            eval = minimax(child, depth - 1, True)
-            minEval = min(minEval, eval)
-        return minEval
+    
+    best_eval = -math.inf if maximizing_player else math.inf
+    for child, i, j in get_children(board, 'X' if maximizing_player else 'O'):
+        child[i][j] = 'X' if maximizing_player else 'O'  # Set the new move
+        eval = minimax(child, depth - 1, not maximizing_player)
+        best_eval = max(best_eval, eval) if maximizing_player else min(best_eval, eval)
+    return best_eval
 
-# Function to make the best move for 'X' (AI)
+# Function to make the best move for 'X'
 def best_move(board):
-    bestVal = -math.inf
+    best_val = -math.inf
     move = None
-    for child, i, j in get_children(board):
-        child[i][j] = 'X'
-        moveVal = minimax(child, 0, False)
-        if moveVal > bestVal:
-            bestVal = moveVal
+    for child, i, j in get_children(board, 'X'):
+        child[i][j] = 'X'  # Set the new move
+        move_val = minimax(child, 0, False)
+        if move_val > best_val:
+            best_val = move_val
             move = (i, j)
     return move
 
-# Function to let the human player make a move
-def human_move(board):
-    while True:
-        try:
-            row = int(input("Enter row (0-2): "))
-            col = int(input("Enter column (0-2): "))
-            if board[row][col] == ' ':
-                board[row][col] = 'O'
-                break
-            else:
-                print("This cell is already occupied. Choose another.")
-        except (ValueError, IndexError):
-            print("Invalid input. Please enter a number between 0 and 2.")
+# Example Tic-Tac-Toe board state
+board = [
+    ['X', 'O', 'X'],
+    [' ', 'X', 'O'],
+    [' ', ' ', 'O']
+]
 
-# Main function to play the game
-def play_game():
-    # Initial empty board
-    board = [[' ' for _ in range(3)] for _ in range(3)]
-    print("Welcome to Tic-Tac-Toe!")
-    print("You are 'O', and the computer is 'X'.")
-    
-    while not game_over(board):
-        # Human player's move
-        display_board(board)
-        print("Your move (O):")
-        human_move(board)
-
-        if game_over(board):
-            break
-
-        # Computer's move
-        print("Computer's move (X):")
-        move = best_move(board)
-        if move:
-            board[move[0]][move[1]] = 'X'
-
-    display_board(board)
-    winner = check_winner(board)
-    if winner == 'X':
-        print("The computer wins!")
-    elif winner == 'O':
-        print("You win!")
-    else:
-        print("It's a draw!")
-
-# Run the game
-play_game()
+# Finding the best move for 'X'
+print("Best move for 'X':", best_move(board))
